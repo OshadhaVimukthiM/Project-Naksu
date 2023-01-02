b4d29aaae44ee57cb0d426b265d4e9e2e18fca02
OshadhaVimukthi <87273124+OshadhaVimukthiM@users.noreply.github.com>
2023-01-03 01:38:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import telebot
+from telebot import types
+import openai
+openai.api_key = "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"
+api = '5821597391:AAG-zGTvptgplCTreo-fpLKW5qJzLQnIkVg'
+bot = telebot.TeleBot(api)
+
+def rsp(question):
+    prmt = "Q: {qst}\nA:".format(qst=question)
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prmt,
+        temperature=0,
+        max_tokens=500,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    return response.choices[0].text
+@bot.message_handler(commands=['start', 'help', 'about'])
+def send_welcome(message):
+ bot.send_message(message.chat.id, '👀')
+ 
+@bot.message_handler(func=lambda message: True) 
+def echo_message(message):
+ msg = message.text
+ #print(msg)
+ response = rsp(msg)
+ #print(response)
+ bot.send_message(message.chat.id, response)
+    
+ 
+print('bot start running')
+bot.polling()
